1dc1697a02f17cc97c693e0525f2d106c58adfc4
Your Name <your@email.com>
2026-07-05 19:54:50 +0530

    Add Vercel Speed Insights

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { HelmetProvider } from 'react-helmet-async'
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 import './index.css'
 import App from './App.jsx'
 
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')).render(
     <HelmetProvider>
       <App />
       <Analytics />
+      <SpeedInsights />
     </HelmetProvider>
   </StrictMode>,
 )
